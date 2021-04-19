6e79795c91c1837600bad48ad3522fc60cdb4aa7
GhostPlays880 <80477077+GhostPlays880@users.noreply.github.com>
2021-04-19 12:32:13 -0400

    added a note

--- Key.java
+++ Key.java
@@ -2,17 +2,40 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
+    public Key(String keyName, String soundFile)
     {
+        key = keyName;
+        sound = soundFile;
     }
-
+    // methods omitted
     /**
      * Do the action for this key.
      */
     public void act()
     {
+        if(!isDown && Greenfoot.isKeyDown("g"))
+        {
+            setImage("white-key-down.png");
+           isDown = true;
+           play();
+        }
+        if (isDown && !Greenfoot.isKeyDown ("g"))
+        { 
+            setImage("white-key.png");
+            isDown = false;
+        }
+    }
+    /*
+     * Play the note of this key
+     */
+    public void play()
+    {
+        Greenfoot.playSound ("3a.wav");
     }
 }
