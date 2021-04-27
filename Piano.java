656bf187d51abbe12b00143c0f851ce5b20aec3c
GhostPlays880 <80477077+GhostPlays880@users.noreply.github.com>
2021-04-27 12:30:21 -0400

    added sounds

--- Piano.java
+++ Piano.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[] whiteKeys =
+    { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\" };
+    private  String[] whiteNotes =
+    { "3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g" };
     /**
      * Make the piano.
      */
@@ -19,10 +23,13 @@ public class Piano extends World
     }
     
     public void makeKeys()
-    {
-        for (int i=0; i<12; i=i+1)
         {
-            addObject (new Key ("g", "3a.wav"), i*63+54, 140);
+            int i;
+            
+            for (i=0; i < whiteKeys.length;  i++)        
+            {                
+                Key key = new Key (whiteKeys[i],  whiteNotes[i] + ".wav");               
+                addObject (key,  54  +  i*63,  140);        
+            }
         }
     }
-}
